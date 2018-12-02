90052b52ce630fcbf55a233521efe49f108dbd53
Vlad <vladfriends@yahoo.ca>
2018-12-02 13:11:55 +0100

    S2L15: added projection to camera, so objects no longer get skewed as they rotate.

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -12,7 +12,7 @@
 const GLint WIDTH = 800, HEIGHT = 600;
 const float toRadians = 3.14159265f / 180.0f;
 
-GLuint VAO, VBO, IBO, shader, uniformModel;
+GLuint VAO, VBO, IBO, shader, uniformModel, uniformProjection;
 
 bool direction = true;
 float triOffset = 0.0f;
@@ -37,14 +37,14 @@ out vec4 vCol;																\n\
 																\n\
 																\n\
 uniform mat4 model;											\n\
-																\n\
+uniform mat4 projection;																\n\
 																\n\
 																\n\
 																\n\
 																\n\
 void main()														\n\
 {																\n\
-	gl_Position = model * vec4(pos, 1.0);	\n\
+	gl_Position = projection * model * vec4(pos, 1.0);	\n\
 																\n\
 	vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);															\n\
 																\n\
@@ -122,6 +122,7 @@ void CompileShaders() {
 	}
 
 	uniformModel = glGetUniformLocation(shader, "model");
+	uniformProjection = glGetUniformLocation(shader, "projection");
 }
 
 void CreateTriangle() {
@@ -214,6 +215,8 @@ int main() {
 	CreateTriangle();
 	CompileShaders();
 
+	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)bufferWidth / (GLfloat)bufferHeight, 0.1f, 100.0f);
+
 	//loop until window closed
 	while (!glfwWindowShouldClose(mainWindow)) {
 		//get and handle user input events
@@ -254,12 +257,13 @@ int main() {
 		glUseProgram(shader);
 		
 			glm::mat4 model;
-			//model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+			model = glm::translate(model, glm::vec3(0.0f, triOffset, -2.5f));
 			model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
 			model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
 
 
 			glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+			glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
 
 			glBindVertexArray(VAO);
 				glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
